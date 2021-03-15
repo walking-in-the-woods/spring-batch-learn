22d7a52e6bc7603c7434c1178d872ff856cd2831
Alex S <as202009261140@gmail.com>
2021-03-16 00:22:02 +0300

    Initial commit

---
+++ src/main/java/as/springbatchlearn/SpringBatchLearnApplication.java
@@ -0,0 +1,13 @@
+package as.springbatchlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/as/springbatchlearn/SpringBatchLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package as.springbatchlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
