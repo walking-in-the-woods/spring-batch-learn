8f28c0af0035fa7d6769793e992f7d125647fdbb
Alex S <as202009261140@gmail.com>
2021-03-20 13:15:21 +0300

    Interface ItemReader. Simple stateless item reader

---
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -0,0 +1,53 @@
+package as.springbatchlearn.configuration;
+
+import as.springbatchlearn.readers.StatelessItemReader;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class JobConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public StatelessItemReader statelessItemReader() {
+        List<String> data = new ArrayList<>(3);
+
+        data.add("Foo");
+        data.add("Bar");
+        data.add("Baz");
+
+        return new StatelessItemReader(data);
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(2)
+                .reader(statelessItemReader())
+                .writer(list -> {
+                    for (String curItem : list) {
+                        System.out.println("curItem = " + curItem);
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Job interfacesJob() {
+        return jobBuilderFactory.get("interfacesJob")
+                .start(step1())
+                .build();
+    }
+}
---
+++ src/main/java/as/springbatchlearn/readers/StatelessItemReader.java
@@ -0,0 +1,24 @@
+package as.springbatchlearn.readers;
+
+import org.springframework.batch.item.ItemReader;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StatelessItemReader implements ItemReader<String> {
+
+    private final Iterator<String> data;
+
+    public StatelessItemReader(List<String> data) {
+        this.data = data.iterator();
+    }
+
+    @Override
+    public String read() throws Exception{
+        if(this.data.hasNext()) {
+            return this.data.next();
+        } else {
+            return null;
+        }
+    }
+}
