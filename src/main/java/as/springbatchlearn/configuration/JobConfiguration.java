5bc1c38595d3b70e137d7252284c0820c649f0e9
Alex S <as202009261140@gmail.com>
2021-03-20 21:26:42 +0300

    Reading from a database. (using jdbc). Restarting a completed step

--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,16 +1,23 @@
 package as.springbatchlearn.configuration;
 
-import as.springbatchlearn.readers.StatelessItemReader;
+import as.springbatchlearn.domain.Customer;
+import as.springbatchlearn.domain.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JobConfiguration {
@@ -21,32 +28,75 @@ public class JobConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Autowired
+    public DataSource dataSource;
+
     @Bean
-    public StatelessItemReader statelessItemReader() {
-        List<String> data = new ArrayList<>(3);
+    public JdbcCursorItemReader<Customer> cursorItemReader() {
+        JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<>();
 
-        data.add("Foo");
-        data.add("Bar");
-        data.add("Baz");
+        // Here, Spring Batch creates a single query
+        reader.setSql("select id, firstName, lastName, birthdate from customer order by lastName, firstName");
+        reader.setDataSource(this.dataSource);
+        reader.setRowMapper(new CustomerRowMapper());
 
-        return new StatelessItemReader(data);
+        return reader;
+    }
+
+    /*
+    Unlike the cursor reader that all we keep track is the number of items that were read,
+    the paging was actually gonna keep track of the last key that was read.
+    So the sort keys for the jdbc paging item reader are important. They have to be unique.
+    In this case first name and last name may not be unique, so we actually sort them by id
+    instead of first name or last name.
+     */
+    @Bean
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);    // actually, it's a page size, it's like a chunk size
+        reader.setRowMapper(new CustomerRowMapper());
+
+        // Here, Spring Batch creates a query for each page
+        PostgresPagingQueryProvider queryProvider = new PostgresPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+
+        return reader;
+    }
+
+    @Bean
+    public ItemWriter<Customer> customerItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(2)
-                .reader(statelessItemReader())
-                .writer(list -> {
-                    for (String curItem : list) {
-                        System.out.println("curItem = " + curItem);
-                    }
-                }).build();
+                .<Customer, Customer>chunk(10)
+//                .reader(cursorItemReader())
+                .reader(pagingItemReader())
+                .writer(customerItemWriter())
+                .allowStartIfComplete(true)
+                .build();
     }
 
     @Bean
-    public Job interfacesJob() {
-        return jobBuilderFactory.get("interfacesJob")
+    public Job job() {
+        return jobBuilderFactory.get("job")
                 .start(step1())
                 .build();
     }
