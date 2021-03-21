8c0e91280b3651660e23799b35a536a1aa5ac8ae
Alex S <as202009261140@gmail.com>
2021-03-21 20:33:10 +0300

    Writing flat files. Custom LineAggregator (in JSON format)

--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,22 +1,25 @@
 package as.springbatchlearn.configuration;
 
 import as.springbatchlearn.domain.Customer;
-import as.springbatchlearn.domain.CustomerFieldSetMapper;
+import as.springbatchlearn.domain.CustomerLineAggregator;
+import as.springbatchlearn.domain.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JobConfiguration {
@@ -31,51 +34,55 @@ public class JobConfiguration {
     public DataSource dataSource;
 
     @Bean
-    public FlatFileItemReader<Customer> customerItemReader() {
-        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
 
-        reader.setLinesToSkip(1);
-        reader.setResource(new ClassPathResource("/data/customer.csv"));
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
 
-        DefaultLineMapper<Customer> customerLineMapper = new DefaultLineMapper<>();
+        PostgresPagingQueryProvider queryProvider = new PostgresPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
 
-        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-        tokenizer.setNames(new String[] {"id", "firstName", "lastName", "birthdate"});
-
-        customerLineMapper.setLineTokenizer(tokenizer);
-        customerLineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
-        customerLineMapper.afterPropertiesSet();
-
-        reader.setLineMapper(customerLineMapper);
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+        reader.setQueryProvider(queryProvider);
 
         return reader;
     }
 
     @Bean
-    public JdbcBatchItemWriter<Customer> customerItemWriter() {
-        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+    public FlatFileItemWriter<Customer> customerItemWriter() throws Exception {
+        FlatFileItemWriter<Customer> itemWriter = new FlatFileItemWriter<>();
 
-        // Execute "TRUNCATE TABLE CUSTOMER" query in the db first (if exists)
-        itemWriter.setDataSource(this.dataSource);
-        itemWriter.setSql("INSERT INTO CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)");
-        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        /*  The line aggregator is an abstraction that allows us to determine how an object is mapped to
+            a string that we've written as a line in our file.
+            The PassThroughLineAggregator calls toString() on each item that is passed in
+        */
+        //itemWriter.setLineAggregator(new PassThroughLineAggregator<>());  // convenient variant (toString())
+        itemWriter.setLineAggregator(new CustomerLineAggregator());         // custom variant (JSON)
+        String customerOutputPath = File.createTempFile("customerOutput", ".out").getAbsolutePath();
+        System.out.println(">> Output Path: " + customerOutputPath);
+        itemWriter.setResource(new FileSystemResource(customerOutputPath));
         itemWriter.afterPropertiesSet();
 
         return itemWriter;
     }
 
     @Bean
-    public Step step1() {
+    public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(10)
-                .reader(customerItemReader())
+                .reader(pagingItemReader())
                 .writer(customerItemWriter())
                 .allowStartIfComplete(true)
                 .build();
     }
 
     @Bean
-    public Job job() {
+    public Job job() throws Exception {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .build();
