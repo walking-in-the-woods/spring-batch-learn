5ef8ded5031a3fd9526528349a8723644438220b
Alex S <as202009261140@gmail.com>
2021-03-21 21:54:13 +0300

    Writing to multiple destination

---
+++ src/main/java/as/springbatchlearn/configuration/CustomerClassifier.java
@@ -0,0 +1,21 @@
+package as.springbatchlearn.configuration;
+
+import as.springbatchlearn.domain.Customer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.classify.Classifier;
+
+public class CustomerClassifier implements Classifier<Customer, ItemWriter<? super Customer>> {
+
+    private ItemWriter<Customer> evenItemWriter;
+    private ItemWriter<Customer> oddItemWriter;
+
+    public CustomerClassifier(ItemWriter<Customer> evenItemWriter, ItemWriter<Customer> oddItemWriter) {
+        this.evenItemWriter = evenItemWriter;
+        this.oddItemWriter = oddItemWriter;
+    }
+
+    @Override
+    public ItemWriter<? super Customer> classify(Customer customer) {
+        return customer.getId() % 2 == 0 ? evenItemWriter : oddItemWriter;
+    }
+}
--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,6 +1,7 @@
 package as.springbatchlearn.configuration;
 
 import as.springbatchlearn.domain.Customer;
+import as.springbatchlearn.domain.CustomerLineAggregator;
 import as.springbatchlearn.domain.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -9,6 +10,8 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.support.ClassifierCompositeItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -54,7 +57,20 @@ public class JobConfiguration {
     }
 
     @Bean
-    public StaxEventItemWriter<Customer> customerItemWriter() throws Exception {
+    public FlatFileItemWriter<Customer> jsonItemWriter() throws Exception {
+        FlatFileItemWriter<Customer> itemWriter = new FlatFileItemWriter<>();
+
+        itemWriter.setLineAggregator(new CustomerLineAggregator());
+        String customerOutputPath = File.createTempFile("customerOutput", ".out").getAbsolutePath();
+        System.out.println(">> Output Path: " + customerOutputPath);
+        itemWriter.setResource(new FileSystemResource(customerOutputPath));
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    @Bean
+    public StaxEventItemWriter<Customer> xmlItemWriter() throws Exception {
         XStreamMarshaller marshaller = new XStreamMarshaller();
 
         Map<String, Class> aliases = new HashMap<>();
@@ -75,13 +91,43 @@ public class JobConfiguration {
         return itemWriter;
     }
 
-    //  For performance reason, it's a good practice to make the chunk size and the fetch size equal
+    // comment this bean if you use ClassifierCompositeItemWriter
+    // CompositeItemWriter implements item stream
+//    @Bean
+//    public CompositeItemWriter<Customer> itemWriter() throws Exception {
+//        List<ItemWriter<? super Customer>> writers = new ArrayList<>(2);
+//
+//        writers.add(xmlItemWriter());
+//        writers.add(jsonItemWriter());
+//
+//        CompositeItemWriter<Customer> itemWriter = new CompositeItemWriter<>();
+//
+//        itemWriter.setDelegates(writers);
+//        itemWriter.afterPropertiesSet();
+//
+//        return itemWriter;
+//    }
+
+    // comment this bean if you use CompositeItemWriter
+    // CompositeItemWriter does not implements item stream, so we need to add streams to the step
+    @Bean
+    public ClassifierCompositeItemWriter<Customer> itemWriter() throws Exception {
+        ClassifierCompositeItemWriter<Customer> itemWriter = new ClassifierCompositeItemWriter<>();
+
+        // even goes to xml, odd goes to json
+        itemWriter.setClassifier(new CustomerClassifier(xmlItemWriter(), jsonItemWriter()));
+
+        return itemWriter;
+    }
+
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(10)
                 .reader(pagingItemReader())
-                .writer(customerItemWriter())
+                .writer(itemWriter())
+                .stream(xmlItemWriter())  // comment this line if you use CompositeItemWriter
+                .stream(jsonItemWriter()) // comment this line if you use CompositeItemWriter
                 .allowStartIfComplete(true)
                 .build();
     }
