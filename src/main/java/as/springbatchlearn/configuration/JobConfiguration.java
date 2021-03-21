3fdd3f0c713336481779830df4a33313973a01ba
Alex S <as202009261140@gmail.com>
2021-03-21 09:09:12 +0300

    ItemStream interface. Completing a job after an error.

--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,21 +1,17 @@
 package as.springbatchlearn.configuration;
 
-import as.springbatchlearn.domain.Customer;
-import as.springbatchlearn.domain.CustomerFieldMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.MultiResourceItemReader;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class JobConfiguration {
@@ -26,48 +22,26 @@ public class JobConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-    @Value("classpath*:/data/*.csv")
-    private Resource[] inputFiles;
-
-    /*
-    The multiResourceItemReader() takes the list of files, and for each file, it will set the resource
-    on the FlatFileItemReader and keep call and read until it returns null.
-    When the reader returns null, it will set the next file and continue on until of the input has been exhausted.
-     */
     @Bean
-    public MultiResourceItemReader<Customer> multiResourceItemReader() {
-        MultiResourceItemReader<Customer> reader = new MultiResourceItemReader<>();
+    @StepScope
+    public StatefulItemReader itemReader() {
+        List<String> items = new ArrayList<>(100);
 
-        // set a delegate who is doing a work of actual reading and parsing files
-        reader.setDelegate(customerItemReader());
-        reader.setResources(inputFiles); // set a collection of resources
+        for(int i = 1; i <= 100; i++) {
+            items.add(String.valueOf(i));
+        }
 
-        return reader;
+        return new StatefulItemReader(items);
     }
 
     @Bean
-    public FlatFileItemReader<Customer> customerItemReader() {
-        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
-
-        DefaultLineMapper<Customer> customerLineMapper = new DefaultLineMapper<>();
-
-        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-        tokenizer.setNames(new String[] {"id", "firstName", "lastName", "birthdate"});
-
-        customerLineMapper.setLineTokenizer(tokenizer);
-        customerLineMapper.setFieldSetMapper(new CustomerFieldMapper());
-        customerLineMapper.afterPropertiesSet();
-
-        reader.setLineMapper(customerLineMapper);
-
-        return reader;
-    }
-
-    @Bean
-    public ItemWriter<Customer> customerItemWriter() {
-        return items -> {
-            for (Customer item : items) {
-                System.out.println(item.toString());
+    public ItemWriter itemWriter() {
+        return new ItemWriter<String>() {
+            @Override
+            public void write(List<? extends String> items) throws Exception {
+                for (String item : items) {
+                    System.out.println(">> " + item);
+                }
             }
         };
     }
@@ -75,15 +49,16 @@ public class JobConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(10)
-                .reader(multiResourceItemReader())
-                .writer(customerItemWriter())
+                .<String, String>chunk(10)
+                .reader(itemReader())
+                .writer(itemWriter())
+                .stream(itemReader())
                 .build();
     }
 
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("statefulJob")
                 .start(step1())
                 .build();
     }
