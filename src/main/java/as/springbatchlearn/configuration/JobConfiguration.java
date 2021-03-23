d85a241862f80ae08008cf8369bb5b6cec3c6c4a
Alex S <as202009261140@gmail.com>
2021-03-23 08:53:22 +0300

    Scaling jobs. Multithreaded step. SimpleAsyncTaskExecutor

--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,21 +1,24 @@
 package as.springbatchlearn.configuration;
 
-import as.springbatchlearn.components.CustomException;
-import as.springbatchlearn.components.CustomSkipListener;
-import as.springbatchlearn.components.SkipItemProcessor;
-import as.springbatchlearn.components.SkipItemWriter;
+import as.springbatchlearn.domain.Customer;
+import as.springbatchlearn.domain.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JobConfiguration {
@@ -26,42 +29,51 @@ public class JobConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Autowired
+    public DataSource dataSource;
+
     @Bean
-    @StepScope
-    public ListItemReader reader() {
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
 
-        List<String> items = new ArrayList<>();
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(1000);
+        reader.setRowMapper(new CustomerRowMapper());
 
-        for(int i = 0; i < 100; i++) {
-            items.add(String.valueOf(i));
-        }
+        PostgresPagingQueryProvider queryProvider = new PostgresPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
 
-        return new ListItemReader<>(items);
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+        reader.setSaveState(false);
+
+        return reader;
     }
 
     @Bean
-    @StepScope
-    public SkipItemProcessor processor() {
-        return new SkipItemProcessor();
-    }
+    public JdbcBatchItemWriter<Customer> customerItemWriter() {
+        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
 
-    @Bean
-    @StepScope
-    public SkipItemWriter writer() {
-        return new SkipItemWriter();
-    }
+        itemWriter.setDataSource(this.dataSource);
+        itemWriter.setSql("INSERT INTO NEW_CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+     }
 
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(10)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                .faultTolerant()
-                .skip(CustomException.class)
-                .skipLimit(15)
-                .listener(new CustomSkipListener())
+                .<Customer, Customer>chunk(1000)
+                .reader(pagingItemReader())
+                .writer(customerItemWriter())
+                .taskExecutor(new SimpleAsyncTaskExecutor()) // not recommended to use in production environment
+                //.allowStartIfComplete(true)
                 .build();
     }
 
---
+++ src/main/java/as/springbatchlearn/domain/CustomerLineAggregator.java
@@ -0,0 +1,23 @@
+package as.springbatchlearn.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.batch.item.file.transform.LineAggregator;
+
+import java.text.SimpleDateFormat;
+
+public class CustomerLineAggregator implements LineAggregator<Customer> {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String aggregate(Customer item) {
+        try {
+            objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            return objectMapper.writeValueAsString(item);
+        }
+        catch (JsonProcessingException e) {
+            throw new RuntimeException("Unable to serialize Customer", e);
+        }
+    }
+}
