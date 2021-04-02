fbbb00101ef59e6b06d2d97983ea1f61a191551e
Alex S <as202009261140@gmail.com>
2021-04-02 22:47:02 +0300

    Remote Chunking

--- src/main/java/as/springbatchlearn/configuration/IntegrationConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/IntegrationConfiguration.java
@@ -1,9 +1,32 @@
 package as.springbatchlearn.configuration;
 
+/*
+    Essentially, what we're going to configure is a round trip. So, we're going to start in our master, we're going to
+    walk through the components out to the slave and then back to the master.
+    In most chunk-based steps you don't need to know the internal components of how Spring Batch manages the chunk.
+    However, when you're dealing with remote chunking, that becomes important, the way a chunk-based step works
+    is actually something called the chunk-oriented tasklet within Spring Batch. So a chunk-based step is actually
+    a tasklet step. The only difference is that has a special tasklet that knows how to handle a chunk.
+    Within the chunk-oriented tasklet there is something called the ChunkHandler. The ChunkHandler is used to handle
+    the item processing in item writing phases of a chunk-based step. So really what happens is the reader reads in
+    a chunks *** of data and it passes it to the chunk handler for processing and writing. We're going to replace
+    that chunk handler with one that knows how to pass that data off to middleware to slaves for processing,
+    and that also how to manage the responses.
+*/
+
+import as.springbatchlearn.domain.Customer;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.batch.core.step.item.SimpleChunkProcessor;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.integration.chunk.ChunkHandler;
+import org.springframework.batch.integration.chunk.ChunkMessageChannelItemWriter;
+import org.springframework.batch.integration.chunk.ChunkProcessorChunkHandler;
+import org.springframework.batch.integration.chunk.RemoteChunkHandlerFactoryBean;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,100 +34,219 @@ import org.springframework.integration.amqp.inbound.AmqpInboundChannelAdapter;
 import org.springframework.integration.amqp.outbound.AmqpOutboundEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.channel.NullChannel;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.messaging.PollableChannel;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.scheduling.support.PeriodicTrigger;
 
 @Configuration
 public class IntegrationConfiguration {
 
-    //  Tis is used in the MessageChannelPartitionHandler to send messages.
+    public static final String CHUNKING_REQUESTS = "chunking.requests";
+    public static final String CHUNKING_REPLIES = "chunking.replies";
+
+    /*
+        We do this with the RemoteChunkHandlerFactoryBean.
+        This factory bean, when Spring Batch finds it will replace the normal chunk handler that is within the
+        chunk tasklet. The factory bean actually uses the ItemWriter to send the messages. So we're going to set that.
+        So the factoryBean.setChunkWriter is going to set a special ItemWriter that is going to write the records out
+        to a middleware. We also configure a step we want to execute. It will use that to introspect to find out
+        what the ItemProcessor and ItemWriter are for the appropriate need. We then return the object.
+    */
     @Bean
-    public MessagingTemplate messagingTemplate() {
+    public ChunkHandler chunkHandler(TaskletStep step1) throws Exception {
+        RemoteChunkHandlerFactoryBean factoryBean = new RemoteChunkHandlerFactoryBean();
+
+        factoryBean.setChunkWriter(chunkWriter());
+        factoryBean.setStep(step1);
+
+        return factoryBean.getObject();
+    }
+
+    /*
+        For our chunking we use this chunk message item writer. This is what's responsible for actually sending
+        the message from the master to the slave. We set the messageTemplate, so we use the Spring Integration
+        Messaging Template for the mechanism of doing the message sending. So, we're going to send that.
+        Within the messageTemplate is also when we're configure the channel that were sending the messages out to.
+        We listen for the responses back on this reply channel (inboundReplies). We'll be sending them out
+        on a channel called outboundRequests. We set max timeouts (it's the maximum number of timeouts we're going to
+        tolerate. And then we return our writer.
+    */
+    @Bean
+    public ChunkMessageChannelItemWriter chunkWriter() {
+        ChunkMessageChannelItemWriter chunkWriter = new ChunkMessageChannelItemWriter();
+
+        chunkWriter.setMessagingOperations(messageTemplate());
+        chunkWriter.setReplyChannel(inboundReplies());
+        chunkWriter.setMaxWaitTimeouts(10);
+
+        return chunkWriter;
+    }
+
+    @Bean
+    public MessagingTemplate messageTemplate() {
         MessagingTemplate messagingTemplate = new MessagingTemplate(outboundRequests());
 
-        messagingTemplate.setReceiveTimeout(60000000L); // 1 hour
+        messagingTemplate.setReceiveTimeout(60000000l);
 
         return messagingTemplate;
     }
 
-    @Bean
-    public DirectChannel outboundRequests() {
-        return new DirectChannel();
-    }
-
-    /*  We're sending messages on to this channel. And this channel is being listen to by the AmqpOutboundEndpoint
-        This peace of code that is going to listen on the outboundRequestChannel, and when the message is coming in
-        it's going to send a message via Rabbit to a particular queue. In this case, it will send it to
-        "partition.request" queue. We'll be listening on that queue in the slave JVMs for work, so we created our
-        AmqpOutboundEndpoint, we configured it properly and returned it.
+    /*
+        We're using Rabbit for our persistent communication in this example.
+        This is what's going to be responsible for actually sending the data from our channel to Rabbit.
     */
     @Bean
     @ServiceActivator(inputChannel = "outboundRequests")
     public AmqpOutboundEndpoint amqpOutboundEndpoint(AmqpTemplate template) {
         AmqpOutboundEndpoint endpoint = new AmqpOutboundEndpoint(template);
 
-        endpoint.setExpectReply(true);
-        endpoint.setOutputChannel(inboundRequests());
-        endpoint.setRoutingKey("partition.requests");
+        endpoint.setExpectReply(false);
+        endpoint.setOutputChannel(inboundReplies());
+
+        endpoint.setRoutingKey(CHUNKING_REQUESTS);
 
         return endpoint;
     }
 
-    // This is the RabbitMQ queue definition
+    // Here we declare our channel itself
     @Bean
-    public Queue requestQueue() {
-        return new Queue("partition.requests", false);
+    public MessageChannel outboundRequests() {
+        return new DirectChannel();
     }
 
-    /*  In the slave JVMs, there are a few additional components.
-        This peace of code is going to be listening for Rabbit to send us messages. That will take the messages off
-        of Rabbit and than pass them onto the internal components in the slave JVMs for processing.
-        We set up our listener container. The listener container is the peace of code that is managing the listening.
-        We set our outboundChannel wherein our sending those messages that we're taking off of Rabbit,
-        in this case it's "inboundRequests". We call afterPropertiesSet and return the adapter.
+    @Bean
+    public Queue requestQueue() {
+        return new Queue(CHUNKING_REQUESTS, false);
+    }
+
+    /*
+        On the slave JVM, we have to listen for the messages the master sends.
+        This is going to receive the messages from Rabbit and write them to this inboundRequestChannel.
+        So, we have outboundRequest in the master and inboundRequest in the slave.
     */
     @Bean
     @Profile("slave")
-    public AmqpInboundChannelAdapter inbound(SimpleMessageListenerContainer listenerContainer) {
+    public AmqpInboundChannelAdapter inboundRequestsAdapter(SimpleMessageListenerContainer listenerContainer) {
         AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
 
         adapter.setOutputChannel(inboundRequests());
+
         adapter.afterPropertiesSet();
 
         return adapter;
     }
 
-    /*  This is that message container. We configured that with a ConnectionFactory to Rabbit.
-        The queue name to listen to in this case is "partition.requests". We set auto start to false.
-        (If we don't set autostart to false we may see examples where we have multiple slave JVMs running
-        and only one is picking up work. So this prevents this condition from happening). Then we return the container.
+    // We declare our message channel
+    @Bean
+    public MessageChannel inboundRequests() {
+        return new DirectChannel();
+    }
+
+    /*
+        We mentioned before that the chunk processor is the peace within Spring Batch that knows how to
+        process a chunk. The ChunkProcessorChunkHandler wraps that so that it can be called when messages arrive
+        from the master. So, every time the message comes in this component knows how to listen for that and calls
+        the SimpleChunkProcessor when the message arrives. So we're going to create our SimpleChunkProcessor
+        which is the exact same component that Spring Batch would use if that was not a remote chunking example.
+        We set our ItemProcessor and ItemWriter, we call for property set, and now we're going to create our
+        ChunkProcessorChunkHandler. This is that wrapper we're just mentioned. We set our chunk processor
+        in the chunk handler, called afterPropertiesSet, and returned chunkHandler.
     */
     @Bean
-    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+    @Profile("slave")
+    @ServiceActivator(inputChannel = "inboundRequests", outputChannel = "outboundReplies")
+    public ChunkProcessorChunkHandler chunkProcessorChunkHandler(ItemProcessor<Customer, Customer> itemProcessor, ItemWriter<Customer> itemWriter) throws Exception {
+        SimpleChunkProcessor chunkProcessor = new SimpleChunkProcessor<>(itemProcessor, itemWriter);
+        chunkProcessor.afterPropertiesSet();
+
+        ChunkProcessorChunkHandler<Customer> chunkHandler = new ChunkProcessorChunkHandler<>();
+
+        chunkHandler.setChunkProcessor(chunkProcessor);
+        chunkHandler.afterPropertiesSet();
+
+        return chunkHandler;
+    }
+
+    @Bean
+    public QueueChannel outboundReplies() {
+        return new QueueChannel();
+    }
+
+    /*
+        On the Reply side of things, once the chunk has been processed, we need to reply that something is happened.
+        And so we'll do that over the outboundReplies channel. And just like in the master, where we had
+        AmqpOutboundEndpoint sending messages from the master to the slave, we've got the same thing sending messages
+        from the slave back to the master. We go ahead to create our endpoint, we configure our routing key to be
+        CHUNKING_REPLIES, (so we have CHUNKING_REQUESTS in the way IN and CHUNKING_REPLIES in the way OUT),
+        and return the endpoint.
+    */
+    @Bean
+    @Profile("slave")
+    @ServiceActivator(inputChannel = "outboundReplies")
+    public AmqpOutboundEndpoint amqpOutboundEndpointReplies(AmqpTemplate template) {
+        AmqpOutboundEndpoint endpoint = new AmqpOutboundEndpoint(template);
+
+        endpoint.setExpectReply(false);
+
+        endpoint.setRoutingKey(CHUNKING_REPLIES);
+
+        return endpoint;
+    }
+
+    @Bean
+    public Queue replyQueue() {
+        return new Queue(CHUNKING_REPLIES, false);
+    }
+
+    // This is out channel of messages coming back in from the slave
+    @Bean
+    public QueueChannel inboundReplies() {
+        return new QueueChannel();
+    }
+
+    /*
+        This is listening to the messages from Rabbit on the master side to be received.
+    */
+    @Bean
+    @Profile("master")
+    public AmqpInboundChannelAdapter inboundRepliesAdapter(SimpleMessageListenerContainer listenerContainer) {
+        AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
+
+        adapter.setOutputChannel(inboundReplies());
+
+        adapter.afterPropertiesSet();
+
+        return adapter;
+    }
+
+    @Bean
+    @Profile("slave")
+    public SimpleMessageListenerContainer requestContainer(ConnectionFactory connectionFactory) {
         SimpleMessageListenerContainer container =
                 new SimpleMessageListenerContainer(connectionFactory);
-
-        container.setQueueNames("partition.requests");
+        container.setQueueNames(CHUNKING_REQUESTS);
         container.setAutoStartup(false);
 
         return container;
     }
 
-    /*  Essentially the way this works is: in a master JVM we have outbound request channel that goes of a Rabbit.
-        Then it's picked up in a slave JVM and it goes on inboundRequests. inboundRequests is listen to by the
-        StepExecutionRequestHandler. The reply of the StepExecutionRequestHandler is in written on the outboundStaging.
-        outboundStaging is a null channel which is actually mean that responses are dropped. Those responses are
-        dropped because we don't need them because we're simply polling the job repository for results.
-    */
     @Bean
-    public PollableChannel outboundStaging() {
-        return new NullChannel();
+    @Profile("master")
+    public SimpleMessageListenerContainer replyContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container =
+                new SimpleMessageListenerContainer(connectionFactory);
+        container.setQueueNames(CHUNKING_REPLIES);
+        container.setAutoStartup(false);
+
+        return container;
     }
 
-    @Bean
-    public QueueChannel inboundRequests() {
-        return new QueueChannel();
+    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+    public PollerMetadata defaultPoller() {
+        PollerMetadata pollerMetadata = new PollerMetadata();
+        pollerMetadata.setTrigger(new PeriodicTrigger(10));
+        return pollerMetadata;
     }
 }
--- src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
+++ src/main/java/as/springbatchlearn/configuration/JobConfiguration.java
@@ -1,50 +1,61 @@
 package as.springbatchlearn.configuration;
 
 /*
-    Remote partitioning allows us to use multiple JVMs (in the cloud for example)
-    The key to this is the MessageChannelPartitionHandler. This implementation of the PartitionHandler interface
-    uses messages to communicate with slaves to execute the slave steps.
-    In our example we'll use RabbitMQ to communicate between the master and the slaves.
+    Within Spring Batch there are two main ways to distribute the work load across multiple JVMs:
+    -   Remote partitioning, which is covered in previous commit, addresses the use cases that are IO-bound.
+        In that slaves are responsible for the input and output, what may be useful in many use cases.
+        But in use cases where the processing itself is the bottleneck and you can not scale the input
+    -   Remote chunking can be a great help. With remote chunking, the input of the chunk-based step is read
+        by the master. Then the items themselves are sent over persistence middleware to slaves for processing.
+        The result can be either returned to the master for writing or written by the slaves themselves.
+        The key difference here between remote partitioning and remote chunking is that in remote partitioning
+        only a description of the data is sent over the wire, the slaves are managed to reading the data themselves.
+        In remote chunking, the master does the reading and sends the data over the wire. So a few of a million
+        items to process with remote partitioning, you can configure how many messages go over the wire by
+        configuring how many partitions there are. With remote chunking, if you have a million items to process,
+        all million of those items are going over the wire.
+*/
 
- */
+/*
+    Running instructions:
 
-import as.springbatchlearn.domain.ColumnRangePartitioner;
+    java -jar Dspring.profiles.active=slave target/spring-batch-learn-0.0.1-SNAPSHOT.jar    // running a slave
+    java -jar Dspring.profiles.active=master target/spring-batch-learn-0.0.1-SNAPSHOT.jar    // running a master
+
+    Adding more slaves increases the all our processing.
+
+    One additional difference between the remote partitioning and remote chunking should be noted.
+    With partitioning there are multiple step executions, one per partition, and one additional one for the master.
+    However with remote chunking, there is just one step execution. Because of this unlike remote partitioning
+    where the state is managed at the slave, state is managed within a master for remote chunking.
+    This means a persistent middleware is required for communication so that items in fly are not lost upon en error.
+
+*/
+
 import as.springbatchlearn.domain.Customer;
 import as.springbatchlearn.domain.CustomerRowMapper;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.partition.PartitionHandler;
-import org.springframework.batch.integration.partition.BeanFactoryStepLocator;
-import org.springframework.batch.integration.partition.MessageChannelPartitionHandler;
-import org.springframework.batch.integration.partition.StepExecutionRequestHandler;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.scheduling.support.PeriodicTrigger;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class JobConfiguration implements ApplicationContextAware {
+public class JobConfiguration {
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -58,89 +69,8 @@ public class JobConfiguration implements ApplicationContextAware {
     @Autowired
     public JobExplorer jobExplorer;
 
-    private ApplicationContext applicationContext;
-
-    private static final int GRID_SIZE = 4;
-
-    /*  The only difference is here. Here we're using a MessageChannelPartitionHandler
-        instead of TaskExecutorPartitionHandler.
-
-        There are two ways for this partition handler to know that all the slave work is done:
-        1.  Listen to messages to be returned. The configuration for that is much more complex.
-        2.  Poll the JobRepository. Since all the slave steps record is step execution for each partition,
-            we can poll the job repository and see if all of those slaves step executions are in the terminal state.
-            (completed, failed, etc). If they are, we know that the slave work is done and we can continue on.
-    */
     @Bean
-    public PartitionHandler partitionHandler(MessagingTemplate messagingTemplate) throws Exception {
-        MessageChannelPartitionHandler partitionHandler = new MessageChannelPartitionHandler();
-
-        partitionHandler.setStepName("slaveStep");  // this is a bean id that we're going to execute on the slave JVMs
-        partitionHandler.setGridSize(GRID_SIZE);
-        partitionHandler.setMessagingOperations(messagingTemplate); // this is the mechanism with which we're sending messages
-        partitionHandler.setPollInterval(5000L);    // configuring the poll interval to be 5 seconds
-        partitionHandler.setJobExplorer(this.jobExplorer);
-        partitionHandler.afterPropertiesSet();
-
-        return partitionHandler;
-    }
-
-    @Bean
-    public ColumnRangePartitioner partitioner() {
-        ColumnRangePartitioner columnRangePartitioner = new ColumnRangePartitioner();
-
-        columnRangePartitioner.setColumn("id");
-        columnRangePartitioner.setDataSource(this.dataSource);
-        columnRangePartitioner.setTable("customer");
-
-        return columnRangePartitioner;
-    }
-
-    /*  The other new peace to this is a StepExecutionRequestHandler.
-        When we execute the slave within a context of a new jvm, it's not being executed within a scope of a job.
-        We're not launching a new job in each slave jvm, we're launching just an independent step.
-        So, we need some way to do that. In this StepExecutionRequestHandler is how we do it.
-
-        This StepExecutionRequestHandler is going to be listening on a Spring Integration channel, in this case
-        "inboundRequests". Every message that comes in is going to execute the step that is requested.
-        In our case it's called "slaveStep". Once that's done, we'll send the response back on this outboundChannel.
-        In our case the outboundChannel is the null channel. It's just going to dropped. We're going to ignore it.
-
-        So to configure this, we create our instance. The StepExecutionRequestHandler needs a way to find
-        that slave step. In our case we're providing the bean id in the request, so we're using this
-        BeanFactoryStepLocator().
-     */
-    @Bean
-    @Profile("slave")
-    @ServiceActivator(inputChannel = "inboundRequests", outputChannel = "outboundStaging")
-    public StepExecutionRequestHandler stepExecutionRequestHandler() {
-        StepExecutionRequestHandler stepExecutionRequestHandler =
-                new StepExecutionRequestHandler();
-
-        BeanFactoryStepLocator stepLocator = new BeanFactoryStepLocator();
-        stepLocator.setBeanFactory(this.applicationContext);    // we're including the context we're running in
-        stepExecutionRequestHandler.setStepLocator(stepLocator);    // we use this to find the step to run
-        stepExecutionRequestHandler.setJobExplorer(this.jobExplorer); // we use this to obey the same context that the
-                                                                    // job would if it was about to launch this step
-                                                                    // so, is this step already running somewhere else.
-                                                                    // It needs to validate that.
-
-        return stepExecutionRequestHandler;
-    }
-
-    @Bean(name = PollerMetadata.DEFAULT_POLLER)
-    public PollerMetadata defaultPoller() {
-        PollerMetadata pollerMetadata = new PollerMetadata();
-        pollerMetadata.setTrigger(new PeriodicTrigger(10));
-        return pollerMetadata;
-    }
-
-    @Bean
-    @StepScope
-    public JdbcPagingItemReader<Customer> pagingItemReader(
-            @Value("#{stepExecutionContext['minValue']}") Long minValue,
-            @Value("#{stepExecutionContext['maxValue']}") Long maxValue) {
-        System.out.println("reading " + minValue + " to " + maxValue);
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
         JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
 
         reader.setDataSource(this.dataSource);
@@ -150,15 +80,21 @@ public class JobConfiguration implements ApplicationContextAware {
         PostgresPagingQueryProvider queryProvider = new PostgresPagingQueryProvider();
         queryProvider.setSelectClause("id, firstName, lastName, birthdate");
         queryProvider.setFromClause("from customer");
-        queryProvider.setWhereClause("where id >= " + minValue + " and id < " + maxValue);
 
         Map<String, Order> sortKeys = new HashMap<>(1);
         sortKeys.put("id", Order.ASCENDING);
         queryProvider.setSortKeys(sortKeys);
-
         reader.setQueryProvider(queryProvider);
 
         return reader;
+    }
+
+    @Bean
+    ItemProcessor<Customer, Customer> upperCaseItemProcessor() {
+        return item -> new Customer(item.getId(),
+                item.getFirstName(),
+                item.getLastName(),
+                item.getBirthdate());
     }
 
     @Bean
@@ -167,42 +103,27 @@ public class JobConfiguration implements ApplicationContextAware {
 
         itemWriter.setDataSource(this.dataSource);
         itemWriter.setSql("INSERT INTO NEW_CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)");
-        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
         itemWriter.afterPropertiesSet();
 
         return itemWriter;
     }
 
     @Bean
-    public Step step1() throws Exception {
+    TaskletStep step1() {
         return stepBuilderFactory.get("step1")
-                .partitioner(slaveStep().getName(), partitioner())
-                .step(slaveStep())
-                .partitionHandler(partitionHandler(null))
-                .allowStartIfComplete(true)
-                .build();
-    }
-
-    @Bean
-    public Step slaveStep() {
-        return stepBuilderFactory.get("slaveStep")
                 .<Customer, Customer>chunk(1000)
-                .reader(pagingItemReader(null, null))
+                .reader(pagingItemReader())
+                .processor(upperCaseItemProcessor())
                 .writer(customerItemWriter())
-                .allowStartIfComplete(true)
                 .build();
     }
 
     @Bean
-    @Profile("master")
+    @Profile("master")  // This is to prevent the job from kicking off in the slave JVMs.
     public Job job() throws Exception {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .build();
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 }
